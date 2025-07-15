50ba10ca723a93a7a8c1b204736b22c054ba4793
Gustavo <gustavo_klahold@estudante.sesisenai.org.br>
2025-07-15 11:22:31 -0300

    add-ex8

---
+++ exercicio08.php
@@ -0,0 +1,47 @@
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <form action="" method="POST">
+
+        <label for="numero">Insira um numero</label>
+        <input type="number" id="numero" name="numero" required>
+
+        <button type="submit" name="verifica_num">Verificar</button>
+
+    </form>
+
+    <?php
+
+    if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+
+        if (isset($_POST['verifica_num'])) {
+
+            $numero = $_POST['numero'];
+
+            $soma = 0;
+
+            for ($i = 1; $i <= ($numero - 1); $i++) {
+
+                if ($i % 2 === 0) {
+
+                    $soma += 1;
+                }
+            }
+            if ($soma == 1) {
+                echo "Existem $soma numero par entre 1 e o $numero";
+            } else {
+                echo "Existem $soma numeros pares entre 1 e o $numero";
+            }
+        };
+    };
+
+    ?>
+</body>
+
+</html>
